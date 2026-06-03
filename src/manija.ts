4c312ccd7d777ef800c6c6a7cd5e74a1aa94afef
Tomas Feld <96779650+tomasFeld@users.noreply.github.com>
2026-06-03 14:09:19 -0300

    Update manija.ts

--- src/manija.ts
+++ src/manija.ts
@@ -1,10 +1,57 @@
 import { preguntar } from './lib/consola.ts';
 
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return 0; // COMPLETAR
+   
+
+  const resultado: number = Number(numero.toString().substring(0, tamañoPrefijo));
+
+  return resultado; 
 }
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let tarjeta: number = parseInt(preguntar('Ingresa el numero de tarjeta: '));
 
-let red: string = ''; // COMPLETAR
-console.log(red);
+let primerosDigitos: number = obtenerPrefijo(tarjeta, 2);
+
+
+switch (primerosDigitos)
+{
+
+  case 34:
+    console.log("AMEX");
+      break;
+    case 37:
+      console.log("AMEX");
+      break;
+      case 51:
+        console.log("MASTER");
+        break;
+        case 52:
+          console.log("MASTER");
+          break;
+          case 53:
+            console.log("MASTER");
+            break;
+            case 54:
+              console.log("MASTER");
+              break;
+              case 55:
+                console.log("MASTER");
+                break;
+
+                default:
+          let primerDigito: number = obtenerPrefijo(tarjeta, 1);
+          if(primerDigito === 4)
+          {
+            console.log("Visa");
+
+          }
+          else
+          {
+            console.log("Prefijo desconocido");
+
+
+          }
+          break;
+
+}
+
